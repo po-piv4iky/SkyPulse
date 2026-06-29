73a3637403a9bd255ab0bf59d897684a19aae9d7
Ivan Petrov <www.ko4ca@gmail.com>
2026-06-29 09:13:47 +0300

    the first animation in the headed has been added

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -1,20 +1,41 @@
 import StyledText from '@/shared/components/StyledText/StyledTex'
 import { THEME } from '@/shared/theme'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { StyleSheet, View } from 'react-native'
+import Animated, {
+  interpolate,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated'
 import Search from './Search/Search'
 
 export default function Header() {
   const [isSearchOpen, setIsSearchOpen] = useState(false)
+  const progress = useSharedValue(0)
+  useEffect(() => {
+    progress.value = withTiming(isSearchOpen ? 1 : 0, {
+      duration: 300,
+    })
+  }, [isSearchOpen])
+  const logoAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(progress.value, [0, 1], [1, 0]),
+      transform: [
+        {
+          translateX: interpolate(progress.value, [0, 1], [0, -25]),
+        },
+      ],
+    }
+  })
   return (
     <View style={styles.headerContainer}>
-      {!isSearchOpen && (
-        <View>
-          <StyledText variant="headline">Sky Pulse</StyledText>
-        </View>
-      )}
+      <Animated.View style={logoAnimatedStyle}>
+        <StyledText variant="headline">Sky Pulse</StyledText>
+      </Animated.View>
+
       <Search
-        isOpen={isSearchOpen}
+        progress={progress}
         onOpen={() => setIsSearchOpen(true)}
         onClose={() => setIsSearchOpen(false)}
       />
@@ -25,11 +46,21 @@ export default function Header() {
 const styles = StyleSheet.create({
   headerContainer: {
     paddingTop: 40,
-    paddingBottom: THEME.spacing.SM,
     paddingHorizontal: THEME.spacing.MD,
-    alignItems: 'center',
-    justifyContent: 'space-between',
     flexDirection: 'row',
-    color: THEME.colors.PRIMARY,
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
 })
+
+// const styles = StyleSheet.create({
+//   headerContainer: {
+//     paddingTop: 40,
+//     paddingBottom: THEME.spacing.SM,
+//     paddingHorizontal: THEME.spacing.MD,
+//     alignItems: 'center',
+//     justifyContent: 'space-between',
+//     flexDirection: 'row',
+//     color: THEME.colors.PRIMARY,
+//   },
+// })
--- src/layouts/Header/Search/Search.tsx
+++ src/layouts/Header/Search/Search.tsx
@@ -1,20 +1,61 @@
+import Animated, {
+  interpolate,
+  SharedValue,
+  useAnimatedStyle,
+} from 'react-native-reanimated'
+
 import StyledButton from '@/shared/components/StyledButton/StyledButton'
 import SearchInput from './SearchInput'
 
-interface SearchProps {
-  isOpen: boolean
+interface Props {
+  progress: SharedValue<number>
   onOpen: () => void
   onClose: () => void
 }
 
-export default function Search({ isOpen, onOpen, onClose }: SearchProps) {
-  if (!isOpen) {
-    return <StyledButton icon="search" onPress={onOpen} />
-  }
+export default function Search({ progress, onOpen, onClose }: Props) {
+  const buttonStyle = useAnimatedStyle(() => ({
+    opacity: interpolate(progress.value, [0, 1], [1, 0]),
 
-  return <SearchInput onClose={onClose} />
-}
+    transform: [
+      {
+        scale: interpolate(progress.value, [0, 1], [1, 0.8]),
+      },
+    ],
+  }))
 
-{
-  /* <SearchButton onOpen={onOpen} /> */
+  const inputStyle = useAnimatedStyle(() => ({
+    width: interpolate(progress.value, [0, 1], [0, 240]),
+
+    opacity: interpolate(progress.value, [0, 1], [0, 1]),
+
+    transform: [
+      {
+        translateX: interpolate(progress.value, [0, 1], [20, 0]),
+      },
+    ],
+  }))
+
+  return (
+    <>
+      <Animated.View
+        style={buttonStyle}
+        pointerEvents={progress.value > 0.5 ? 'none' : 'auto'}
+      >
+        <StyledButton icon="search" onPress={onOpen} />
+      </Animated.View>
+
+      <Animated.View
+        style={[
+          {
+            overflow: 'hidden',
+          },
+          inputStyle,
+        ]}
+        pointerEvents={progress.value < 0.5 ? 'none' : 'auto'}
+      >
+        <SearchInput onClose={onClose} />
+      </Animated.View>
+    </>
+  )
 }
